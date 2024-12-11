da9c9b1276e54357c3e32c62bc588573f8709f7d
KazakuAlina <alinakazaku49@gmail.com>
2024-12-11 14:10:08 +0500

    Добавлена точка

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGameExample {
+    internal static class Program {
+        /// <summary>
+        /// Главная точка входа для приложения1212.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(true);
+            Application.Run(new FrmSnakeGame());
+        }
+    }
+}
